33b43b4841aa07da99e69b2af5a0c602f1fdeee2
ashish <kathiriyaashish433@gmail.com>
2026-06-24 16:01:42 +0530

    Refactor CORS middleware usage in app.js

--- src/app.js
+++ src/app.js
@@ -29,9 +29,7 @@ const allowedOrigins = [
   "http://localhost:5173",
 ];
 
-app.use(
-
-  cors({
+app.use(cors({
     origin: function (origin, callback) {
       if (!origin) {
         return callback(null, true);
@@ -48,7 +46,7 @@ app.use(
   })
 );
 
-app.options("*", cors());
+app.options(/.*/, cors());
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 
